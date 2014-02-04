1dacd346a75d61893b54b39b6b8c9fc7c7aabad7
KMoon <kmoon@convey.com>
2014-02-04 15:23:31 -0600

    move mk_regex

--- build/build.cmd
+++ build/build.cmd
@@ -7,5 +7,5 @@ IF ERRORLEVEL 1 EXIT /B
 msbuild ..\..\convey-public-libs-proj\kbmmemtable\kbmmemtable_d11.dproj /p:Configuration=%1
 IF ERRORLEVEL 1 EXIT /B
 
-msbuild ..\..\convey-public-libs-proj\Mk_RegEX_d11.dproj /p:Configuration=%1
+msbuild ..\..\convey-public-libs-proj\Mk_RegEx\Mk_RegEX_d11.dproj /p:Configuration=%1
 IF ERRORLEVEL 1 EXIT /B
