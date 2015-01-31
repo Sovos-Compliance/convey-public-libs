5d92f1b8d3f38127849a1a90df0563dd42bcdf10
Jose Sebastian Battig <jsbattig@convey.com>
2015-01-30 19:28:11 -0600

    B-0 added thread safety

--- uThreadSafeXorShiftRandom.pas
+++ uThreadSafeXorShiftRandom.pas
@@ -10,27 +10,47 @@ var
 
 implementation
 
+uses
+  uSpinLock;
+
+var
+  Lock : TSpinLock;
+
 function Random : Int64;
 var
   x, y : Int64;
 begin
-  x := RandSeed[0];
-  y := RandSeed[1];
-  RandSeed[0] := y;
-  x := x xor (x shl 23);
-  x := x xor (x shr 17);
-  x := x xor (y xor (y shr 26));
-  RandSeed[1] := x;
+  Lock.Enter;
+  try
+    x := RandSeed[0];
+    y := RandSeed[1];
+    RandSeed[0] := y;
+    x := x xor (x shl 23);
+    x := x xor (x shr 17);
+    x := x xor (y xor (y shr 26));
+    RandSeed[1] := x;
+  finally
+    Lock.Leave;
+  end;
   Result := x + y;
 end;
 
 procedure Randomize;
 begin
   System.Randomize;
-  RandSeed[0] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
-  RandSeed[1] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
+  Lock.Enter;
+  try
+    RandSeed[0] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
+    RandSeed[1] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
+  finally
+    Lock.Leave;
+  end;
 end;
 
 initialization
+  Lock := TSpinLock.Create;
+  Lock.SupportReentrantLocks := False;
   Randomize;
+finalization
+  Lock.Free;
 end.
