267ff9ca028bb84a81367eb0182ab9e693e84e18
Jose Sebastian Battig <jsbattig@convey.com>
2015-02-02 16:52:22 -0600

    B-0 made randseed private

--- uThreadSafeXorShiftRandom.pas
+++ uThreadSafeXorShiftRandom.pas
@@ -1,56 +1,59 @@
+{  Thread-safe Xorshift+ generator by  Saito and Matsumoto }
+
 unit uThreadSafeXorShiftRandom;
 
 interface
 
 function Random : Int64;
 procedure Randomize;
-
-var
-  RandSeed : array[0..1] of Int64;
+procedure SetRandSeed(ALowPart, AHighPart : Int64);
 
 implementation
 
 uses
-  uSpinLock;
+  Windows, SysUtils;
+
+resourcestring
+  SHighPartAndLowPartOfRandSeedCantBeEqual = 'HighPart and LowPart of RandSeed can''t be equal';
 
 var
-  Lock : TSpinLock;
+  RandSeed : array[0..1] of Int64;
 
 function Random : Int64;
 var
   x, y : Int64;
 begin
-  Lock.Enter;
-  try
+  repeat
     x := RandSeed[0];
     y := RandSeed[1];
-    RandSeed[0] := y;
-    x := x xor (x shl 23);
-    x := x xor (x shr 17);
-    x := x xor (y xor (y shr 26));
-    RandSeed[1] := x;
-  finally
-    Lock.Leave;
-  end;
+  until (x <> y) and (InterlockedCompareExchange64(RandSeed[0], y, x) = x);
+  x := x xor (x shl 23);
+  x := x xor (x shr 17);
+  x := x xor (y xor (y shr 26));
+  RandSeed[1] := x;
   Result := x + y;
 end;
 
 procedure Randomize;
+var
+  x, y : Int64;
 begin
   System.Randomize;
-  Lock.Enter;
-  try
-    RandSeed[0] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
-    RandSeed[1] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
-  finally
-    Lock.Leave;
-  end;
+  repeat
+    x := RandSeed[0];
+    y := RandSeed[1];
+  until (x <> y) and (InterlockedCompareExchange64(RandSeed[0], Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32), x) = x);
+  RandSeed[1] := Int64(System.Random(MaxInt)) or (Int64(System.Random(MaxInt)) shl 32);
+end;
+
+procedure SetRandSeed(ALowPart, AHighPart : Int64);
+begin
+  if ALowPart = AHighPart then
+    raise Exception.Create(SHighPartAndLowPartOfRandSeedCantBeEqual);
 end;
 
 initialization
-  Lock := TSpinLock.Create;
-  Lock.SupportReentrantLocks := False;
+  RandSeed[0] := 1;
+  RandSeed[1] := 2;
   Randomize;
-finalization
-  Lock.Free;
 end.
