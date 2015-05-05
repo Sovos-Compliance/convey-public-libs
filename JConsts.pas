78c93a2d1c1b854531c8a2ff297a19085c1d1a6e
Jose Sebastian Battig <jsbattig@convey.com>
2015-05-05 11:36:33 -0500

    B-0 added missing unit from VCL/RTL in Delphi 2007

---
+++ JConsts.pas
@@ -0,0 +1,22 @@
+
+{*******************************************************}
+{                                                       }
+{       CodeGear Delphi Runtime Library                 }
+{                                                       }
+{  	    Copyright (c) 1995-2008 CodeGear	        }
+{                                                       }
+{*******************************************************}
+
+unit JConsts;
+
+interface
+
+resourcestring
+  sChangeJPGSize = 'Cannot change the size of a JPEG image';
+  sJPEGError = 'JPEG error #%d';
+  sJPEGImageFile = 'JPEG Image File';
+  
+implementation
+
+end.
+ 
