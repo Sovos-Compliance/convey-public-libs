2f5e104ebb64c50d8591daefdc1c0169296c3e04
Eugene Bubelo <ebubelo@convey.com>
2015-10-21 23:16:51 -0500

    B-0: created failing test

---
+++ Tests/TestDeCAL.pas
@@ -0,0 +1,29 @@
+unit TestDeCAL;
+
+interface
+
+uses
+  TestFramework, DeCAL;
+
+type
+  {$M+}
+  TestDeCALUnit = class(TTestCase)
+  published
+    procedure Test;
+  end;
+
+implementation
+
+{ TestLogObject }
+
+procedure TestDeCALUnit.Test;
+var
+  Sequence: ISequence;
+begin
+  Sequence := Factory.CreateContainer(STR_LIST) as ISequence;
+end;
+
+initialization
+  RegisterTest(TestDeCALUnit.Suite);
+
+end.
